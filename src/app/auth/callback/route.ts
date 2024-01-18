07d647b0801140f78eafebeb8dfa2353061db2d3
Edwin A. Espinal Taveras <71328952+EdwinEspi18@users.noreply.github.com>
2024-01-18 12:31:54 -0400

    consolel

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -6,6 +6,7 @@ import {NextResponse} from "next/server";
 
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
+  console.log(requestUrl)
   const code = requestUrl.searchParams.get("code");
 
   if (code) {
